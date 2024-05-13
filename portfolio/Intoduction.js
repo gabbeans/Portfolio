93b85e5aa88c1a0b867c51f06568835df56f0da4
gabbeans <hello.gabbyy@outlook.com>
2024-05-13 15:01:47 -0700

    Intro

---
+++ portfolio/Intoduction.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Introduction = () => {
+  return (
+    <section id="introduction">
+      <div className="container">
+        <h2>Introduction</h2>
+        <p>I'm Gabby, a software developer from Los Angeles, California.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Introduction;
